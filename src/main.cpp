9050e7d352ee71c29d5683308b8b0545e61ab46b
EbbeVang <ev@evang.dk>
2020-10-01 21:26:51 +0200

    adds project to git

---
+++ src/main.cpp
@@ -0,0 +1,63 @@
+#include <Arduino.h>
+#include "WiFi.h"
+#include "AsyncUDP.h"
+
+const char * ssid = "wifi ssid";
+const char * password = "wifi password";
+
+AsyncUDP udp;
+
+void setup()
+{
+    Serial.begin(9600);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+        Serial.println("WiFi Failed");
+        while(1) {
+            delay(1000);
+        }
+    }
+    if(udp.listen(4000)) {
+        Serial.print("UDP Listening on IP: ");
+        Serial.println(WiFi.localIP());
+        udp.onPacket([](AsyncUDPPacket packet) {
+            Serial.print("UDP Packet Type: ");
+            Serial.print(packet.isBroadcast()?"Broadcast":packet.isMulticast()?"Multicast":"Unicast");
+            Serial.print(", From: ");
+            Serial.print(packet.remoteIP());
+            Serial.print(":");
+            Serial.print(packet.remotePort());
+            Serial.print(", To: ");
+            Serial.print(packet.localIP());
+            Serial.print(":");
+            Serial.print(packet.localPort());
+            Serial.print(", Length: ");
+            Serial.print(packet.length());
+            Serial.print(", Data: ");
+            Serial.write(packet.data(), packet.length());
+            Serial.println();
+            
+            //reply to the client/sender
+            packet.printf("Got %u bytes of data", packet.length());
+        
+        });
+    }
+
+        //Send unicast
+        //udp.print("Hello Server!");
+        //udp.
+        
+    }
+
+
+void loop()
+{   
+    delay(5000);
+
+    //Send broadcast on port 4000
+    udp.broadcastTo("Anyone here?", 4000);
+
+    //Serial.println("waiting for udp message...");
+    udp.writeTo((const uint8_t*)"Hej med dig", 11, IPAddress(192,168,1,3), 54321);
+}
