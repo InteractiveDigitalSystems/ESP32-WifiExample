d61815ee4f0a2db1243ce7aba7db5945bb35be98
faaip <frederik@tollund.cc>
2020-10-08 15:57:25 +0200

    send string in function

--- src/main.cpp
+++ src/main.cpp
@@ -7,57 +7,61 @@ const char * password = "wifi password";
 
 AsyncUDP udp;
 
-void setup()
-{
-    Serial.begin(9600);
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, password);
-    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-        Serial.println("WiFi Failed");
-        while(1) {
-            delay(1000);
-        }
+void setup() {
+  Serial.begin(9600);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("WiFi Failed");
+    while (1) {
+      delay(1000);
     }
-    if(udp.listen(4000)) {
-        Serial.print("UDP Listening on IP: ");
-        Serial.println(WiFi.localIP());
-        udp.onPacket([](AsyncUDPPacket packet) {
-            Serial.print("UDP Packet Type: ");
-            Serial.print(packet.isBroadcast()?"Broadcast":packet.isMulticast()?"Multicast":"Unicast");
-            Serial.print(", From: ");
-            Serial.print(packet.remoteIP());
-            Serial.print(":");
-            Serial.print(packet.remotePort());
-            Serial.print(", To: ");
-            Serial.print(packet.localIP());
-            Serial.print(":");
-            Serial.print(packet.localPort());
-            Serial.print(", Length: ");
-            Serial.print(packet.length());
-            Serial.print(", Data: ");
-            Serial.write(packet.data(), packet.length());
-            Serial.println();
-            
-            //reply to the client/sender
-            packet.printf("Got %u bytes of data", packet.length());
-        
-        });
-    }
+  }
+  if (udp.listen(4000)) {
+    Serial.print("UDP Listening on IP: ");
+    Serial.println(WiFi.localIP());
+    udp.onPacket([](AsyncUDPPacket packet) {
+      Serial.print("UDP Packet Type: ");
+      Serial.print(packet.isBroadcast()
+                       ? "Broadcast"
+                       : packet.isMulticast() ? "Multicast" : "Unicast");
+      Serial.print(", From: ");
+      Serial.print(packet.remoteIP());
+      Serial.print(":");
+      Serial.print(packet.remotePort());
+      Serial.print(", To: ");
+      Serial.print(packet.localIP());
+      Serial.print(":");
+      Serial.print(packet.localPort());
+      Serial.print(", Length: ");
+      Serial.print(packet.length());
+      Serial.print(", Data: ");
+      Serial.write(packet.data(), packet.length());
+      Serial.println();
 
-        //Send unicast
-        //udp.print("Hello Server!");
-        //udp.
-        
-    }
+      // reply to the client/sender
+      packet.printf("Got %u bytes of data", packet.length());
+    });
+  }
 
+  // Send unicast
+  // udp.print("Hello Server!");
+  // udp.
+}
 
-void loop()
-{   
-    delay(5000);
+void sendMessage(String msg){
+  udp.writeTo((const uint8_t *)msg.c_str(), msg.length(),
+              IPAddress(192, 168, 1, 3), 54321);
+}
 
-    //Send broadcast on port 4000
-    udp.broadcastTo("Anyone here?", 4000);
+void loop() {
+  delay(5000);
 
-    //Serial.println("waiting for udp message...");
-    udp.writeTo((const uint8_t*)"Hej med dig", 11, IPAddress(192,168,1,3), 54321);
+  // Send broadcast on port 4000
+  udp.broadcastTo("Anyone here?", 4000);
+
+  // Serial.println("waiting for udp message...");
+  int x = 100;
+  int y = 100;
+  String sendMessage("init" + String(x) + " " + String(y));
 }
